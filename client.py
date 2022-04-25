ec57d1d87a1f356a2b2d88a3c3da62940ac227bb
JordanNewkirk <79227183+JordanNewkirk@users.noreply.github.com>
2022-04-24 21:35:52 -0700

    Add files via upload

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 import os
 import hashlib
 
-def hash_file(filename):
+def hash_file(filename): #This is a function to get the hash name of the file that is sent over. This might have to be used in the server? I'm not sure but it grabs the hash name for the file and I have it outputing just to see that it is working.
 	h = hashlib.sha1()
 	
 	with open(filename, 'rb') as file:
@@ -10,33 +10,39 @@ def hash_file(filename):
 		while chunk != b'':
 			chunk = file.read(1024)
 			h.update(chunk)
-	return h.hexdigest()
+	return h.hexdigest() 
 
 IP = socket.gethostbyname(socket.gethostname())  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+PORT = 4455  # The port used by the server
 ADDR = (IP, PORT)
 FORMAT = "utf-8"
 SIZE = 1024
 
 def main():
-
+	#Starting the TCP socket
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	
+	client.connect(ADDR) #Connecting to the server
 
-	client.connect(ADDR)
-
-	file = open("test.txt", "r")
+	file = open("test.txt", "r") #Opening and reading the file data.
 
 	data = file.read()
 
-	client.send("test.txt".encode(FORMAT))
+	client.send("test.txt".encode(FORMAT)) #Sending the filename to the server
 
-	msg = client.recv(SIZE).decode(FORMAT)
+	msg = client.recv(SIZE).decode(FORMAT) #Updating the server message
+
+	print(f"[SERVER]: {msg}")
+	
+	client.send(data.encode(FORMAT)) #Sending the file data to the server
+	msg = client.recv(SIZE).decode(FORMAT) #Updating the server message
 
 	print(f"[SERVER]: {msg}")
 
-	file.close()
-	client.close()
-
+	file.close() #Closing the file
+	client.close() #Closing the connection
+	
+	#More UDP stuff I was messing around with.
 	#client_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	#client_UDP.connect(ADDR)
 
@@ -55,7 +61,7 @@ def main():
 
 
 	hash_message = hash_file("test.txt")
-	print(f"Hash Message: {hash_message}")
+	print(f"Hash Message: {hash_message}") #Here is just an output showing the hash message from the file to show it. But we'll need it to compare it with the UDP transfer.
 
 main()
 
--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import socket
 import os
 
-IP = socket.gethostbyname(socket.gethostname())  # Standard loopback interface address (localhost)
-SERVER_PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+IP = socket.gethostbyname(socket.gethostname())  #(localhost)
+SERVER_PORT = 4455  # Port to listen on (non-privileged ports are > 1023)
 ADDR = (IP, SERVER_PORT)
 SIZE = 1024
 FORMAT = "utf-8"
@@ -10,27 +10,29 @@ FORMAT = "utf-8"
 def main():
 
 	print("[STARTING] Server is starting.")
-
-	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server.bind(ADDR)
-	server.listen()
+	#Starting TCP Connection
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Starting the socket
+	server.bind(ADDR) #Bind the IP and SERVER_PORT to the server
+	server.listen() #Server is listening(waiting for client)
 
 	print("[LISTENING] Server is listening.")
 
 	while True:
-
+		#Server has accepted the connection from the client.
 		conn, addr = server.accept()
 
 		print(f"[NEW CONNECTION] {addr} connected.")
 
-		filename = conn.recv(SIZE).decode(FORMAT)
+		filename = conn.recv(SIZE).decode(FORMAT) #Recieving the filename from the client.
 
 		print(f"[RECV] Receiving the filename.")
 
 		file = open(filename, "w")
 
 		conn.send("Filename recieved.".encode(FORMAT))
+		print(f"Filename: {filename}")
 
+		#recieving file data from the client
 		data = conn.recv(SIZE).decode(FORMAT)
 
 		print(f"[RECV] Receiving the file data.")
@@ -38,13 +40,13 @@ def main():
 		file.write(data)
 
 		conn.send("File data received.".encode(FORMAT))
+		
+		file.close() #Closing the file
 
-		file.close()
-
-		conn.close()
+		conn.close() #Closing the connection from the client.
 
 		print(f"[DISCONNECTED] {addr} disconnected.")
-	
+	#This is where I was messing with the UDP stuff kind of lol. It's not that great... it was messing up the code. 
 	#UDP_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	#UDP_server.bind(ADDR)
 	
