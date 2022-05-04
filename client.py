e4c5b5eed01e83c687c0ae8edc931d41fe78fde9
Albert Galang <albrtglng@gmail.com>
2022-05-04 14:32:06 -0700

    comments

--- client.py
+++ client.py
@@ -17,15 +17,16 @@ def main():
 	#      is sent.
 	TCPClient.send(ADDR, FORMAT, SIZE)
 
+	file = open("test.txt", "r")  # Opening and reading the file data.
+	data = file.read()
+	file.close()  # Closing the file
+
 	# UDP: The client will send the file to the server after the TCP procedures.
 	#      After sending the data, the server will check the acquired data's
 	#      SHA256 value with the other value sent using the TCP protocol.
 	#      A match will mean the packets were sent reliably.
 	#      In the event that there is no match, the client will resend the data
 	#      to the server.
-	file = open("test.txt", "r")  # Opening and reading the file data.
-	data = file.read()
-	file.close()  # Closing the file
 	UDPClient.send(ADDR, FORMAT, SIZE, data)
 
 
