fac216ef1d2e2bb63143c179225ffe3a5d71e47f
Albert Galang <albrtglng@gmail.com>
2022-05-04 13:42:30 -0700

    comments

--- server.py
+++ server.py
@@ -19,7 +19,11 @@ def main():
     #      name and the SHA256 value to be checked later.
     filename, hash_message = TCPServer.start_server(ADDR, FORMAT, SIZE)
 
-    # UDP:
+    # UDP: Start server and listen for incoming datagrams.
+    #      The SHA256 of the acquired data is taken.
+    #      The acquired data SHA256 is checked against the SHA256 hash_message
+    #      value received in the TCP transfer. A match means the packets
+    #      were sent reliably.
     message = UDPServer.start_server(ADDR, FORMAT, SIZE, hash_message)
     fileWrite = open('received.txt', 'wb')
     fileWrite.write(message)
